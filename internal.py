2e7a903a24780f2418b4ce15b3f5c516d0149561
BenitzCoding <benitzcoding@yahoo.com>
2022-07-12 16:27:59 +0800

    Added more Internal Properties.

--- internal.py
+++ internal.py
@@ -4,9 +4,19 @@ from cool_utils import Terminal
 
 class Internal:
     def __init__(self):
+        self.bot = None
+        self.owner = None
         self.config = None
         self.application_id = None
         self.token = None
+        self.core_guild = None
+        self.message_channel = None
+        self.join_channel = None
+        self.ads_channel = None
+        self.alerts_channel = None
+
+    def pass_bot(self, bot):
+        self.bot = bot
 
     def error(self, error: Exception) -> None:
         Terminal.error(f"{error}")
@@ -23,8 +33,14 @@ class Internal:
 
         async with open(file, 'r') as file_:
             self.config = json.load(file_)
+            self.owner = self.bot.get_user(self.config.get("owner"))
             self.application_id = self.config.get("application_id")
             self.token = self.config.get("token")
+            self.core_guild = await self.bot.fetch_channel(self.config.get("core_guild"))
+            self.message_channel = await self.bot.fetch_channel(self.config.get("message_channel"))
+            self.join_channel = await self.bot.fetch_channel(self.config.get("join_channel"))
+            self.ads_channel = self.config.get("ads_channel")
+            self.alerts_channel = self.config.get("alerts_channel")
     
     async def setup(self) -> None:
         if self.config is None:
